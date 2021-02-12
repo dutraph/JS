ed60d8665ee8717607162d02585561f278aadf19
Paulo Dutra <dutra.phs@gmail.com>
2021-02-12 01:25:06 -0300

    add

--- 1-Fundamentals-Part-1/script.js
+++ 1-Fundamentals-Part-1/script.js
@@ -7,3 +7,12 @@ firstName = "maria";
 
 
 console.log(firstName)
+
+function fun(){
+    console.log("this is a funcition")
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
