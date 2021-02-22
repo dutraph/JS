e47dc28bacc08345466d52cc4a35fe8410314705
tmaior <tmaior@ezops.com.br>
2021-02-22 19:52:53 -0300

    refactoring

--- Course_Projects/01-Guess-My-Name-DEV/script.js
+++ Course_Projects/01-Guess-My-Name-DEV/script.js
@@ -6,83 +6,107 @@
     DOM = WEB APIs !== Javascript
 */
 
-// Functions section
-function newHighScore() {
-  return docQuerySelText('.highscore', highScore());
-}
 
-function Score() {
-  score--;
-  docQuerySelText('.score', score);
-}
-
-function highScore() {
-  if (score > highscore) {
-    highscore = score;
-    docQuerySelText('.highscore', highscore);
-  }
-}
-
-const docQuerySelText = (element, message) =>
-  (document.querySelector(element).textContent = message);
-
-const docQuerySelValue = (element, message) =>
-  (document.querySelector(element).value = message);
-
-function styleWin() {
-  document.querySelector('body').style.backgroundColor = '#60b347';
-  document.querySelector('.number').style.width = '30rem';
-  document.querySelector('.number').style.fontSize = '12rem';
-}
-
-function styleDefault() {
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
-  document.querySelector('.number').style.fontSize = '6rem';
-}
-
+/**
+ * MUTABLES
+ */
 // add +1 to the end to make sure that will be > 0
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
 
-  // No input
-  if (!guess) {
-    docQuerySelText('.message', '🚨 No Number!');
 
-    // Player wins
-  } else if (guess === secretNumber) {
-    docQuerySelText('.message', '🎉  Nice Shot 🎉');
-    docQuerySelText('.number', secretNumber);
-    highScore();
 
-    // CSS
-    styleWin();
+/**
+ * FUNCTIONS
+ */
+const querySelector = document.querySelector.bind(document)
+const body = querySelector('body')
 
-    // guess to high
-  } else if (guess !== secretNumber) {
-    if (score > 1) {
-      docQuerySelText(
-        '.message',
-        guess > secretNumber ? '⤴  To high...' : '⤵  To low...'
-      );
-      Score();
-    } else {
-      docQuerySelText('.score', 0);
-      docQuerySelText('.message', 'Game Over');
-    }
-  }
-});
+const newHighScore = () => docQuerySelText('.highscore', highScore());
+const docQuerySelText = (element, message) => querySelector(element).textContent = message;
+const docQuerySelValue = (element, message) => querySelector(element).value = message;
 
-//Play again button
-document.querySelector('.again').addEventListener('click', function () {
+const Score = () => {
+  score--;
+  docQuerySelText('.score', score);
+}
+
+const highScore = () => {
+  if (score <= highscore) return;
+
+  highscore = score;
+  docQuerySelText('.highscore', highscore);
+}
+
+const styleWin = () => {
+  const $number = querySelector('.number')
+
+  $number.style.width = '30rem';
+  $number.style.fontSize = '12rem';
+
+  body.style.backgroundColor = '#60b347';
+}
+
+const styleDefault = () => {
+  const $number = querySelector('.number')
+
+  $number.style.width = '15rem';
+  $number.style.fontSize = '6rem';
+
+  body.style.backgroundColor = '#222';
+}
+
+const restartGame = () => {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
+
   docQuerySelText('.message', 'Start guessing...');
   docQuerySelText('.score', score);
   docQuerySelText('.number', '?');
   docQuerySelValue('.guess', '');
+
   styleDefault();
-});
+}
+
+const gameWin = () => {
+  // Player wins
+  docQuerySelText('.message', '🎉  Nice Shot 🎉');
+  docQuerySelText('.number', secretNumber);
+  highScore();
+
+  // CSS
+  styleWin();
+}
+
+const gameOver = () => {
+  docQuerySelText('.score', 0);
+  docQuerySelText('.message', 'Game Over');
+}
+
+const gameGuess = (message) => {
+  Score();
+  docQuerySelText('.message', message);
+}
+
+querySelector('.check')
+  .addEventListener('click', function () {
+    const guess = +querySelector('.guess').value;
+
+    if (!guess) return docQuerySelText('.message', '🚨 No Number!');
+
+    if (guess === secretNumber) return gameWin()
+
+    if (score <= 1) return gameOver()
+
+    console.log(1)
+
+    gameGuess(
+      guess > secretNumber
+        ? '⤴  To high...'
+        : '⤵  To low...'
+    )
+  });
+
+//Play again button
+querySelector('.again').addEventListener('click', restartGame);
