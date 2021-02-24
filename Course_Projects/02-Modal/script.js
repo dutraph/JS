98dbf4a698e277f12561b261ff50e6407ab3140b
Paulo Dutra <dutra.phs@gmail.com>
2021-02-24 20:02:48 -0300

    Finish project no. 2 (Modal Windows)

--- Course_Projects/02-Modal/script.js
+++ Course_Projects/02-Modal/script.js
@@ -4,8 +4,26 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnClose = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal'); // we have 3 buttons
-console.log(btnsOpenModal);
+
+// Using function expression (anonymous function) we can set as parameter for the addEventLIstener
+const openModal = function () {
+  modal.classList.remove('hidden'); // for this classes we dont use '.' before, only in selectors
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
+  btnClose.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
 }
+// 3 keyboad events (keydown / keyup / Keypress)
+document.addEventListener('keydown', function (e) {
+  if (e.key == 'Escape') {
+    closeModal();
+  }
+});
