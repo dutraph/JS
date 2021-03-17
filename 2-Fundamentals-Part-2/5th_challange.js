cceb4a6a3a7c3fdde3bdeb0dbf5336f0a89df04f
Paulo Dutra <dutra.phs@gmail.com>
2021-03-17 19:12:02 -0300

    refactor 5th_challange

--- 2-Fundamentals-Part-2/5th_challange.js
+++ 2-Fundamentals-Part-2/5th_challange.js
@@ -1,30 +1,15 @@
-// const calcTip = bill =>{
-//     if (bill <=300  && bill >=50){
-//         const tip = bill * 0.15;
-//         return tip;
-//     }else{
-//         const tip = bill * 0.20;
-//         return tip;
-//     }
-// }
-
-// or use ternary
-const calcTip = bill => bill <=300  && bill >=50 ? bill * 0.15 : bill * 0.2;
-
-const bill = [125, 555, 44];
+const bills = [122, 555, 44];
 const tips = [];
 const total = [];
 
-for (let t = 0; t < bill.length; t++) {
-    tips.push(calcTip(bill[t]));
-    
-}
-for (let i = 0; i < bill.length; i++) {
-    total.push(bill[i] + tips[i]);
+const calcTip = billValue =>
+  billValue >= 50 && billValue <= 300 ? billValue * 0.15 : billValue * 0.2;
+
+for (let i = 0; i < bills.length; i++) {
+  tips.push(calcTip(bills[i]));
+  total.push(tips[i] + bills[i]);
 }
 
-console.log(`Bills: ${bill}`);
+console.log(`Bills: ${bills}`);
 console.log(`Tips: ${tips}`);
 console.log(`Total: ${total}`);
-
-
